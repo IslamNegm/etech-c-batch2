806a758810c663d77baf94f2e1d4d69555e67e37
IslamNegm <islam.negm@valeo.com>
2019-09-04 22:17:50 +0200

    adding session 4 last example

---
+++ session-04/04-multiple-file-hello-world/src/hello.c
@@ -0,0 +1,13 @@
+/*
+ * hello.c
+ *
+ *  Created on: Aug 31, 2019
+ *      Author: inegm
+ */
+#include <stdio.h>
+void helloworld(void)
+{
+
+	printf("Hello world\n");
+}
+
---
+++ session-04/04-multiple-file-hello-world/src/hello.h
@@ -0,0 +1,8 @@
+/*
+ * hello.h
+ *
+ *  Created on: Aug 31, 2019
+ *      Author: inegm
+ */
+
+void helloworld(void);
